56950002cc148325e693d81f1531417690da2099
pgrepds <david.scholz@pgrepds.io>
2022-05-31 19:07:56 +0200

    add lunarvim initial configuration

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -2,12 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="~/.npm-global/bin:$PATH"
-
-# Created by `pipx` on 2022-02-18 00:26:07
-set PATH $PATH /home/pgrepds/.local/bin
-
 # Aliases
 alias gita='python3 -m gita'
 alias vim='nvim'
@@ -17,3 +11,6 @@ zoxide init fish | source
 
 # Starship shell prompt
 # starship init fish | source
+
+set PATH /home/$USER/.local/bin $PATH
+set PATH /home/$USER/.npm-global/bin $PATH
